4edb9ed3e100f6059520dea4073420e801ba8ed8
chitian-victor <152418595+chitian-victor@users.noreply.github.com>
2025-03-04 00:26:58 +0800

    Update errgroup.go

--- errgroup.go
+++ errgroup.go
@@ -1,3 +1,8 @@
+// Package chitian/errgroup is an extension of the sync/errgroup which has this header comment:
+//
+// Copyright 2016 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
 package errgroup
 
 import (
