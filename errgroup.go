3e308847c07c41136a502f23e4599e24d0e3694b
victor <chitian.victor@gmail.com>
2025-03-04 22:31:57 +0800

    feat: update test unit

--- errgroup.go
+++ errgroup.go
@@ -6,12 +6,11 @@
 package errgroup
 
 import (
-        "context"
-        "errors"
-        "runtime"
-        "sync"
-
-        "code.byted.org/gopkg/logs"
+	"context"
+	"errors"
+	"log"
+	"runtime"
+	"sync"
 )
 
 // A Group is a collection of goroutines working on subtasks that are part of
@@ -19,57 +18,57 @@ import (
 //
 // A zero Group is valid and does not cancel on error.
 type Group struct {
-        doneOnce sync.Once
-        done     chan struct{}
+	doneOnce sync.Once
+	done     chan struct{}
 
-        wg      sync.WaitGroup
-        errOnce sync.Once
-        err     error
+	wg      sync.WaitGroup
+	errOnce sync.Once
+	err     error
 
-        ch chan struct{}
+	ch chan struct{}
 }
 
 // default cancel on error.
 func WithCancel() *Group {
-        return &Group{done: make(chan struct{})}
+	return &Group{done: make(chan struct{})}
 }
 
 // Not Recommended, just to be compatibility with package "golang.org/x/sync/errgroup"
 func WithContext(ctx context.Context) (*Group, context.Context) {
-        return &Group{done: make(chan struct{})}, ctx
+	return &Group{done: make(chan struct{})}, ctx
 }
 
 // set max goroutine to work.
 func (g *Group) SetLimit(n int) {
-        g.ch = make(chan struct{}, n)
+	g.ch = make(chan struct{}, n)
 }
 
 // close channel only once
 func (g *Group) cancel() {
-        if g.done == nil {
-                return
-        }
-        g.doneOnce.Do(func() {
-                close(g.done)
-        })
+	if g.done == nil {
+		return
+	}
+	g.doneOnce.Do(func() {
+		close(g.done)
+	})
 }
 
 func (g *Group) done_() {
-        g.wg.Done()
-        if g.ch != nil {
-                <-g.ch
-        }
+	g.wg.Done()
+	if g.ch != nil {
+		<-g.ch
+	}
 }
 
 // Wait blocks until all function calls from the Go method have returned, then
 // returns the first non-nil error (if any) from them.
 func (g *Group) Wait() error {
-        g.wg.Wait()
-        g.cancel()
-        if g.ch != nil {
-                close(g.ch)
-        }
-        return g.err
+	g.wg.Wait()
+	g.cancel()
+	if g.ch != nil {
+		close(g.ch)
+	}
+	return g.err
 }
 
 // Go calls the given function in a new goroutine.
@@ -77,47 +76,44 @@ func (g *Group) Wait() error {
 // The first call to return a non-nil error cancels the group; its error will be
 // returned by Wait.
 func (g *Group) Go(f func() error) {
-        if g.ch != nil {
-                select {
-                case <-g.done:
-                        return
-                case g.ch <- struct{}{}:
-                        break
-                }
-        }
+	if g.ch != nil {
+		select {
+		case <-g.done:
+			return
+		case g.ch <- struct{}{}:
+			break
+		}
+	}
 
-        g.wg.Add(1)
-        go func() {
-                defer g.done_()
-                localDone := make(chan error, 2)
-                go func() {
-                        defer func() {
-                                if err := recover(); err != nil {
-                                        const size = 64 << 10
-                                        buf := make([]byte, size)
-                                        buf = buf[:runtime.Stack(buf, false)]
-                                        logs.Error("panic: err=%s\n%s",
-                                                logs.SecMark("error", err),
-                                                logs.SecMark("stack", string(buf)))
-                                        localDone <- errors.New("panic")
-                                }
-                                close(localDone)
-                        }()
-                        if err := f(); err != nil {
-                                localDone <- err
-                        }
-                }()
+	g.wg.Add(1)
+	go func() {
+		defer g.done_()
+		localDone := make(chan error, 2)
+		go func() {
+			defer func() {
+				if err := recover(); err != nil {
+					const size = 64 << 10
+					buf := make([]byte, size)
+					buf = buf[:runtime.Stack(buf, false)]
+					log.Printf("panic: err=%s\n%s", err, string(buf))
+					localDone <- errors.New("panic")
+				}
+				close(localDone)
+			}()
+			if err := f(); err != nil {
+				localDone <- err
+			}
+		}()
 
-                select {
-                case err := <-localDone:
-                        if err != nil {
-                                g.errOnce.Do(func() {
-                                        g.err = err
-                                })
-                                g.cancel()
-                        }
-                case <-g.done: // when g.done is nil, this case will be blocked
-                }
-        }()
+		select {
+		case err := <-localDone:
+			if err != nil {
+				g.errOnce.Do(func() {
+					g.err = err
+				})
+				g.cancel()
+			}
+		case <-g.done: // when g.done is nil, this case will be blocked
+		}
+	}()
 }
-
--- errgroup_test.go
+++ errgroup_test.go
@@ -1,107 +1,109 @@
 package errgroup
+
 import (
-    "context"
-    "errors"
-    "testing"
-    "time"
-    . "github.com/smartystreets/goconvey/convey"
-    . "github.com/bytedance/mockey"
+	"context"
+	"errors"
+	. "github.com/bytedance/mockey"
+	. "github.com/smartystreets/goconvey/convey"
+	"testing"
+	"time"
 )
+
 func GetTestFunc(t *testing.T, name string, sleepTime time.Duration, ret error, HasPanic bool) func() error {
-    return func() error {
-       //t.Logf(" %v start", name)
-       time.Sleep(sleepTime)
-       if HasPanic {
-          panic("panic occur")
-       }
-       //t.Logf(" %v end", name)
-       return ret
-    }
+	return func() error {
+		//t.Logf(" %v start", name)
+		time.Sleep(sleepTime)
+		if HasPanic {
+			panic("panic occur")
+		}
+		//t.Logf(" %v end", name)
+		return ret
+	}
 }
 func TestErrgroup(t *testing.T) {
-    type Field struct {
-       name         string
-       funcList     []func() error
-       maxGoroutine int
-       wantMaxTime time.Duration
-       wantMinTime time.Duration
-    }
-    fields := []Field{
-       {
-          name: "normal",
-          funcList: []func() error{
-             GetTestFunc(t, "f-1s-nil", time.Second, nil, false),
-             GetTestFunc(t, "f-2s-nil", time.Second*2, nil, false),
-             GetTestFunc(t, "f-3s-nil", time.Second*3, nil, false),
-          },
-          wantMaxTime: time.Second*3 + time.Millisecond*50,
-          wantMinTime: time.Second * 3,
-       },
-       {
-          name: "one error",
-          funcList: []func() error{
-             GetTestFunc(t, "f-2s-nil", time.Second*2, nil, false),
-             GetTestFunc(t, "f-1s-err", time.Second, errors.New("failed"), false),
-             GetTestFunc(t, "f-300ms-nil", time.Millisecond*300, nil, false),
-          },
-          wantMaxTime: time.Second + time.Millisecond*50,
-          wantMinTime: time.Second,
-       },
-       {
-          name: "one panic",
-          funcList: []func() error{
-             GetTestFunc(t, "f-2s-nil", time.Second*2, nil, false),
-             GetTestFunc(t, "f-1s-nil-panic", time.Second, nil, true),
-             GetTestFunc(t, "f-300ms-nil", time.Millisecond*300, nil, false),
-          },
-          wantMaxTime: time.Second + time.Millisecond*50,
-          wantMinTime: time.Second,
-       },
-       {
-          name: "normal and max_goroutine=2",
-          funcList: []func() error{
-             GetTestFunc(t, "f-1s-nil", time.Second, nil, false),
-             GetTestFunc(t, "f-1s-nil", time.Second, nil, false),
-             GetTestFunc(t, "f-1s-nil", time.Second, nil, false),
-          },
-          maxGoroutine: 2,
-          wantMaxTime:  time.Second*2 + time.Millisecond*50,
-          wantMinTime:  time.Second * 2,
-       },
-       {
-          name: "one error and max_goroutine=2",
-          funcList: []func() error{
-             GetTestFunc(t, "f-1s-nil", time.Second*2, nil, false),
-             GetTestFunc(t, "f-1s-err", time.Second, nil, false),
-             GetTestFunc(t, "f-1s-nil", time.Millisecond*300, errors.New("failed"), false),
-          },
-          maxGoroutine: 2,
-          wantMaxTime:  time.Second*1 + time.Millisecond*350,
-          wantMinTime:  time.Second*1 + time.Millisecond*300,
-       },
-    }
-    for _, field := range fields {
-       t.Run(field.name, func(t *testing.T) {
-          PatchConvey(field.name, t, func() {
-             now := time.Now()
-             var interval time.Duration
-             t.Logf("%v: start-time=%v", field.name, now)
-             ctx := context.Background()
-             g, ctx := WithContext(ctx)
-             if field.maxGoroutine != 0 {
-                g.SetLimit(field.maxGoroutine)
-             }
-             for _, f := range field.funcList {
-                g.Go(f)
-             }
-             if wErr := g.Wait(); wErr != nil {
-                t.Logf("%v err=%v", field.name, wErr)
-             }
-             interval = time.Now().Sub(now)
-             So(interval, ShouldBeGreaterThanOrEqualTo, field.wantMinTime)
-             So(interval, ShouldBeLessThan, field.wantMaxTime)
-             t.Logf("%v: end-time=%v\ninterval=%v", field.name, time.Now(), interval)
-          })
-       })
-    }
+	type Field struct {
+		name         string
+		funcList     []func() error
+		maxGoroutine int
+		wantMaxTime  time.Duration
+		wantMinTime  time.Duration
+	}
+	fields := []Field{
+		{
+			name: "normal",
+			funcList: []func() error{
+				GetTestFunc(t, "f-1s-nil", time.Second, nil, false),
+				GetTestFunc(t, "f-2s-nil", time.Second*2, nil, false),
+				GetTestFunc(t, "f-3s-nil", time.Second*3, nil, false),
+			},
+			wantMaxTime: time.Second*3 + time.Millisecond*50,
+			wantMinTime: time.Second * 3,
+		},
+		{
+			name: "one error",
+			funcList: []func() error{
+				GetTestFunc(t, "f-2s-nil", time.Second*2, nil, false),
+				GetTestFunc(t, "f-1s-err", time.Second, errors.New("failed"), false),
+				GetTestFunc(t, "f-300ms-nil", time.Millisecond*300, nil, false),
+			},
+			wantMaxTime: time.Second + time.Millisecond*50,
+			wantMinTime: time.Second,
+		},
+		{
+			name: "one panic",
+			funcList: []func() error{
+				GetTestFunc(t, "f-2s-nil", time.Second*2, nil, false),
+				GetTestFunc(t, "f-1s-nil-panic", time.Second, nil, true),
+				GetTestFunc(t, "f-300ms-nil", time.Millisecond*300, nil, false),
+			},
+			wantMaxTime: time.Second + time.Millisecond*50,
+			wantMinTime: time.Second,
+		},
+		{
+			name: "normal and max_goroutine=2",
+			funcList: []func() error{
+				GetTestFunc(t, "f-1s-nil", time.Second, nil, false),
+				GetTestFunc(t, "f-1s-nil", time.Second, nil, false),
+				GetTestFunc(t, "f-1s-nil", time.Second, nil, false),
+			},
+			maxGoroutine: 2,
+			wantMaxTime:  time.Second*2 + time.Millisecond*50,
+			wantMinTime:  time.Second * 2,
+		},
+		{
+			name: "one error and max_goroutine=2",
+			funcList: []func() error{
+				GetTestFunc(t, "f-1s-nil", time.Second*2, nil, false),
+				GetTestFunc(t, "f-1s-err", time.Second, nil, false),
+				GetTestFunc(t, "f-1s-nil", time.Millisecond*300, errors.New("failed"), false),
+			},
+			maxGoroutine: 2,
+			wantMaxTime:  time.Second*1 + time.Millisecond*350,
+			wantMinTime:  time.Second*1 + time.Millisecond*300,
+		},
+	}
+	for _, field := range fields {
+		t.Run(field.name, func(t *testing.T) {
+			PatchConvey(field.name, t, func() {
+				now := time.Now()
+				var interval time.Duration
+				t.Logf("%v: start-time=%v", field.name, now)
+				ctx := context.Background()
+				g, ctx := WithContext(ctx)
+				if field.maxGoroutine != 0 {
+					g.SetLimit(field.maxGoroutine)
+				}
+				for _, f := range field.funcList {
+					g.Go(f)
+				}
+				if wErr := g.Wait(); wErr != nil {
+					t.Logf("%v err=%v", field.name, wErr)
+				}
+				interval = time.Now().Sub(now)
+				So(interval, ShouldBeGreaterThanOrEqualTo, field.wantMinTime)
+				So(interval, ShouldBeLessThan, field.wantMaxTime)
+				t.Logf("%v: end-time=%v\ninterval=%v", field.name, time.Now(), interval)
+			})
+		})
+	}
 }
